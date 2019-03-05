770eafc45979e17a1bb4a3c2f79062b164a53494
Jakub Turek <jkbturek@gmail.com>
2019-03-05 20:29:29 +0100

    Add help command to the project

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSimpleCLI",
-            dependencies: []),
+            dependencies: ["Commandant", "SwiftSyntax"]),
         .testTarget(
             name: "SwiftSimpleCLITests",
             dependencies: ["SwiftSimpleCLI"]),
--- Sources/SwiftSimpleCLI/main.swift
+++ Sources/SwiftSimpleCLI/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Commandant
+import Foundation
+
+enum MainError: Error {}
+
+let registry = CommandRegistry<MainError>()
+
+let helpCommand = HelpCommand(registry: registry)
+registry.register(helpCommand)
+
+registry.main(defaultVerb: helpCommand.verb) { error in
+    fputs(error.localizedDescription + "\n", stderr)
+}
