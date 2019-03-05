35a4d67283ff06791601a250b4950dac3f9b8ea6
Jakub Turek <jkbturek@gmail.com>
2019-03-05 20:19:41 +0100

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftSimpleCLI",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.40200.0")),
+        .package(url: "https://github.com/Carthage/Commandant.git", .exact("0.15.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 1. `export TOOLCHAINS=swift`
 2. `swift --version`
 3. `swift package init --type executable`
-
+4. `swift package generate-xcodeproj`
+5. Add `swift-syntax` & `Commandant` to `Package.swift`.
+6. `swift package generate-xcodeproj` again
+7. 
